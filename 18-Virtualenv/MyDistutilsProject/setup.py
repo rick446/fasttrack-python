47139552c3058f15f7060028707d4b2726ef3111
Rick Copeland <rick@arborian.com>
2012-10-09 18:48:30 +0800

    Add virtualenv/packaging
    Signed-off-by: Rick Copeland <rick@arborian.com>

---
+++ 18-Virtualenv/MyDistutilsProject/setup.py
@@ -0,0 +1,25 @@
+from distutils.core import setup
+
+version = '0.0'
+
+setup(name='MyDistutilsProject',
+      version=version,
+      description="",
+      long_description="""\
+""",
+      classifiers=[], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+      keywords='',
+      author='',
+      author_email='',
+      url='',
+      license='',
+      packages=['mydistutilsproject'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          # -*- Extra requirements: -*-
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+      """,
+      )
